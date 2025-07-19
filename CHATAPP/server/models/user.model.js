9019ee3f0deb0789d012876cc94828cde8ba2669
alsongard <alsongard@gmail.com>
2025-07-19 14:07:34 +0300

    user model added & done

---
+++ CHATAPP/server/models/user.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: {type:String, required:true, unique:true, trim:true },
+        password: {type:String, required:true, trim:true}
+    },
+    {timestamps: true}
+)
+
+const User = mongoose.model('User', UserSchema)
+module.exports = User;
+
+// mongoose.model() first argument is used to provide the collection name
