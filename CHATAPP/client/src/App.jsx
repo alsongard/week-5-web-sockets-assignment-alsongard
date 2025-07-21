98038dbeb2df25b4521d611994a07be4a508a4a4
alsongard <alsongard@gmail.com>
2025-07-21 22:14:11 +0300

    browserrouter, darkmode set fully functional

--- CHATAPP/client/src/App.jsx
+++ CHATAPP/client/src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './components/header'
+import HomePage from './components/homeComponent';
+import Footer from './components/footer';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import LoginPage from "./pages/loginPage";
+import RegisterPage from "./pages/register";
+import ChatPage from './pages/chatPage';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App()
+{
+  const[darkMode, setDarkMode] = useState("");
+  useEffect(()=>{
+    const themeResult = window.matchMedia("(prefers-color-scheme:dark)").matches
+    console.log(themeResult);
+    setDarkMode(themeResult);
 
+  },[])
+  
+  const bg = darkMode ? "dark": ""
+  console.log(`bg is : ${bg}`)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={`${bg}`}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div><Header/><Outlet/><Footer/></div>}>
+            <Route index element={<HomePage/>}/>
+            <Route path="login" element={<LoginPage/>}/>
+            <Route path="register" element={<RegisterPage/>}/>
+            <Route path="chat" element={<ChatPage/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      
+    </div>
   )
 }
 
